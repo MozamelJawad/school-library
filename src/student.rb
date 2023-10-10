47a3d06ea242d5db6fed44953ffc24f8dd4f515c
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-10 14:22:14 +0430

    Create student.rb file and Student class

---
+++ src/student.rb
@@ -0,0 +1,19 @@
+require_relative 'person'
+
+class Student < Person
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
+
+
+st1 = Student.new(22, 2, 'Jawad',true)
+puts st1.play_hooky
+
+st1 = Student.new(22, 2)
+puts st1.play_hooky
