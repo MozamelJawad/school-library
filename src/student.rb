ee8dc1e550868c89933e801d518391f814084797
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-11 22:48:41 +0430

    modify the class based on the requirements

--- src/student.rb
+++ src/student.rb
@@ -4,15 +4,10 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
   end
 end
-
-st1 = Student.new(22, 2, 'Jawad', parent_permission: true)
-puts st1.play_hooky
-
-st2 = Student.new(22, 2)
-puts st2.play_hooky
