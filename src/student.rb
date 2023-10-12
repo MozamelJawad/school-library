73a517c499b2a36300f2635e80a72b069bf894e0
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-12 21:30:42 +0430

    modify the class

--- src/student.rb
+++ src/student.rb
@@ -1,10 +1,15 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission: parent_permission)
+  attr_accessor :classroom
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
